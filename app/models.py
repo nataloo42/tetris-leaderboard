3ccb25b0c44b5ad6651071ca76f6043e04152532
Keith Clements <keith.clements89@gmail.com>
2024-03-01 11:20:00 -0600

    This commit adds an "admin" area for us to add/remove/edit players. Currently, it is only accessible to authorized users. To login, just go to /login and sign in. This can be made more sophisticated if we want to give everyone the option of creating a password (and maybe giving everyone a "profile" page with their stats?)

--- app/models.py
+++ app/models.py
@@ -1,18 +1,29 @@
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db
+from app import db, login
 from datetime import datetime, timezone
 from flask_login import UserMixin
-# from app import login
 from hashlib import md5
 
 
 class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    first_name: so.Mapped[int] = so.mapped_column(sa.String(30))
-    last_name: so.Mapped[int] = so.mapped_column(sa.String(30))    
+    username: so.Mapped[Optional[str]] = so.mapped_column(sa.String(30), index=True,
+                                                unique=True)
+    first_name: so.Mapped[Optional[str]] = so.mapped_column(sa.String(30))
+    last_name: so.Mapped[Optional[str]] = so.mapped_column(sa.String(30))
     rating: so.Mapped[int] = so.mapped_column(sa.Integer)
+    wins: so.Mapped[Optional[int]] = so.mapped_column(sa.Integer)
+    losses: so.Mapped[Optional[int]] = so.mapped_column(sa.Integer)
+    password: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
 
     def __repr__(self):
         return '<{} {}>'.format(self.first_name, self.last_name)
+
+    def check_password(self, password):
+        return password == self.password
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,57 @@
-from flask import url_for
+from flask_basicauth import BasicAuth
 from app import app
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from flask_login import current_user, login_user
 import sqlalchemy as sa
 from app import db
 from app.models import User
-# from flask_login import logout_user, login_required
+from app.forms import LoginForm
+from flask_admin.contrib.sqla import ModelView
+from flask_admin import AdminIndexView, Admin
+from werkzeug.exceptions import HTTPException
+from werkzeug import Response
 from flask import request
 from urllib.parse import urlsplit
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 def index():
     query = sa.select(User)
     users = db.session.scalars(query).all()
-    headers = ['First Name', 'Last Name', 'Rating']
+    headers = ['First Name', 'Last Name', 'Rating', 'Wins', 'Losses']
     return render_template('index.html', users=users, headers=headers)
+
+# Basic login page. Not necessary to view basic /index content but we could use this to
+# add more user-specific features later.
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user)
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+
+# This class supercedes ModelView to restrict access to authenticated users only
+class MyModelView(ModelView):
+    def is_accessible(self):
+        return current_user.is_authenticated
+
+    def inaccessible_callback(self, name, **kwargs):
+        # redirect to login page if user doesn't have access
+        return redirect(url_for('login', next=request.url))
+
+# Use the built-in flask-admin to create the /admin domain
+# This is currently only accessible to kclements user
+# If more users need to use it in the future, this can be easily changed
+admin = Admin(app, name="Admin")
+admin.add_view(MyModelView(User, db.session))
